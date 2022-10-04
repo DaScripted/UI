095664f847113279c1275726c49ea803bf28067f
DaScripted <114707978+DaScripted@users.noreply.github.com>
2022-10-04 02:10:47 +0100

    Update UI.lua

--- UI.lua
+++ UI.lua
@@ -64,11 +64,11 @@ local themeStyles = {
         ElementColor = Color3.fromRGB(20, 20, 20)
     },
     LightTheme = {
-        SchemeColor = Color3.fromRGB(74,65,156), --changed custom
+        SchemeColor = Color3.fromRGB(31, 26, 93), --changed custom
         Background = Color3.fromRGB(255,94,40), --changed custom
-        Header = Color3.fromRGB(245,54,7), --changed custom
+        Header = Color3.fromRGB(74,65,156), --changed custom
         TextColor = Color3.fromRGB(219,249,255), --changed custom
-        ElementColor = Color3.fromRGB(31, 26, 93) --changed custom
+        ElementColor = Color3.fromRGB(245,54,7) --changed custom
     },
     BloodTheme = {
         SchemeColor = Color3.fromRGB(227, 27, 27),
